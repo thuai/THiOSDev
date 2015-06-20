5ed776e7ec97d155a6945f041b8fd5b1d610f6a1
Terry Tang <thuai.cn@gmail.com>
2015-06-20 23:07:05 +0800

    add example project

---
+++ Example/THiOSDev-example-Swift/THiOSDev-example-SwiftTests/THiOSDev_example_SwiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  THiOSDev_example_SwiftTests.swift
+//  THiOSDev-example-SwiftTests
+//
+//  Created by thuai on 15/6/20.
+//  Copyright © 2015年 thuai Technology Co., Ltd. All rights reserved.
+//
+
+import XCTest
+
+class THiOSDev_example_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
